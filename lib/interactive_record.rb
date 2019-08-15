9d95f728b29b6eefc472a1ca8d305ac0b126987b
Eric Clark <eclark0013@gmail.com>
2019-08-15 15:24:01 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -13,6 +13,11 @@ class InteractiveRecord
 
     table_info = DB[:conn].execute(sql)
     binding.pry
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
   end
 
 end
